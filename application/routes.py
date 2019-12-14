d027db2add7f43c3bce9601deadceecc880b9b4e
d <d>
2019-12-14 14:33:26 +0100

    changed routed to rest api format

--- application/routes.py
+++ application/routes.py
@@ -7,29 +7,57 @@ from flask import jsonify
 
 @app.route('/stops', methods=['GET'])
 def stops():
-    return render_template('stops.html',
-                           stops=Stop.query.all(),
-                           title="Show Stops")
+
+    stopss = Stop.query.all()
+
+    output = []
+
+    for sstop in stopss:
+        stop_data = {}
+        stop_data['name'] = sstop.stop_name
+        output.append(stop_data)
+
+    return jsonify({'stops': output})
 
 
 @app.route('/lines', methods=['GET'])
 def lines():
-    return render_template('lines.html',
-                           lines=Line.query.all(),
-                           title="Show lines")
+    lines = Line.query.all()
+
+    output = []
+
+    for line in lines:
+        line_data = {}
+        line_data['name'] = line.line_name
+        output.append(line_data)
+
+    return jsonify({'lines': output})
+#
+# @app.route('/stops', methods=['GET'])
+# def stops():
+#     return render_template('stops.html',
+#                            stops=Stop.query.all(),
+#                            title="Show Stops")
 
 
-@app.route('/stop', methods=['GET'])
-def stop():
-    # stps = []
-    wer = Stop.query.all()
-    result_list = []
-    for row in wer:
-        result_list.append({
-            'id': wer.id,
-            'name': wer.stop_name
-            })
-    return row
+# @app.route('/lines', methods=['GET'])
+# def lines():
+#     return render_template('lines.html',
+#                            lines=Line.query.all(),
+#                            title="Show lines")
+#
+#
+# @app.route('/stop', methods=['GET'])
+# def stop():
+#     # stps = []
+#     wer = Stop.query.all()
+#     result_list = []
+#     for row in wer:
+#         result_list.append({
+#             'id': wer.id,
+#             'name': wer.stop_name
+#             })
+#     return row
 
 # class HelloWorld(Resource):
 #     def get(self):
