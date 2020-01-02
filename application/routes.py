791f3d9913b32a804667568db66451c6abb1afb1
d <d>
2020-01-02 12:08:44 +0100

    routes /user/add, /lines/line, favourites

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,10 @@
 from flask import request, render_template, make_response
 from datetime import datetime as dt
 from flask import current_app as app
-from .models import db, User, Stop, Line
+from .models import db, Users, Stop, Line
 from flask import jsonify
+from datetime import datetime
+from flask_restful import Resource, Api
 
 
 @app.route('/stops', methods=['GET'])
@@ -17,7 +19,7 @@ def stops():
         stop_data['name'] = sstop.stop_name
         output.append(stop_data)
 
-    return jsonify({'stops': output})
+    return jsonify({'stops': output}), 200
 
 
 @app.route('/lines', methods=['GET'])
@@ -32,6 +34,45 @@ def lines():
         output.append(line_data)
 
     return jsonify({'lines': output})
+
+
+@app.route('/user/add', methods=['POST'])
+def add_user():
+    id = request.form['id1']
+    id2 = request.form['id2']
+    onboard = request.form['on_type']
+    os_type = request.form['os_type']
+
+    new_user = Users(id, id2, onboard, os_type, datetime.now())
+    db.session.add(new_user)
+    db.session.commit()
+
+    return 'User added to DB', 201
+
+
+@app.route('/lines/line/<line_name>', methods=['GET'])
+def line(line_name):
+    line_1 = Line.query.filter_by(line_name=line_name).first()
+    return str(line_1)
+
+
+@app.route('/favourites/line', methods=['POST'])
+def favourite_lines():
+    d = request.json
+    # store received data in DB
+    #
+    #
+    return "Line has been added to favourites", 201
+
+
+@app.route('/favourites/stop', methods=['POST'])
+def favourite_stops():
+    d = request.json
+    # store received data in DB
+    #
+    #
+    return "Stop has been added to favourites", 201
+
 #
 # @app.route('/stops', methods=['GET'])
 # def stops():
