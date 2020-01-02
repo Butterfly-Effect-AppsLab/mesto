39761a345edcc86cf888660ec315cff75d7f3168
Svoboda <svoboda@essential-data.sk>
2020-01-02 17:58:04 +0100

    AWS setup

--- application/__init__.py
+++ application/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=False)
+    application = app = Flask(__name__, instance_relative_config=False)
     # app.config.from_object('config.Config')
 
     app.config['DEBUG'] = True
